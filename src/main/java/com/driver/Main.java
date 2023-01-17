c1c9cc2239d15202e9454fabd3d9de3ca0d0811f
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2023-01-17 16:12:00 +0530

    First Try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,22 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println("Base Price Of The Pizza: "+dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
-
-    Pizza p = new Pizza(true);
-    System.out.println("Base Price Of The Pizza: "+p.getPrice());
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+//    DeluxePizza dp = new DeluxePizza(true);
+//    System.out.println("Base Price Of The Pizza: "+dp.getPrice());
+//    dp.addTakeaway();
+//    dp.addExtraCheese();
+//    dp.addTakeaway();
+//    System.out.println(dp.getBill());
+//
+//    Pizza p = new Pizza(true);
+//    System.out.println("Base Price Of The Pizza: "+p.getPrice());
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addTakeaway();
+//    System.out.println(p.getBill());
+    DeluxePizza a=new DeluxePizza(false);
+    System.out.println(a.getBill());
+    DeluxePizza b=new DeluxePizza(true);
+    System.out.println(b.getBill());
   }
 }
